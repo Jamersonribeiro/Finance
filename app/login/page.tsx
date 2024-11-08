6a639ffcb69dca34efb40bf60e91ea616c8c7787
Seu Nome <seuemail@exemplo.com>
2024-11-08 07:44:16 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,38 @@
+import { LogInIcon } from "lucide-react";
+import Image from "next/image";
+
+const LoginPage = () => {
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/* ESQUERDA */}
+      <div className="max-w-[550]px mx-auto flex h-full flex-col justify-center p-10">
+        <Image
+          src="/logo.png"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <button variant="outline">Fazer login ou criar conta</button>
+      </div>
+
+      {/* DIREITA */}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça Login"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+const Transactions = () => {
+  return <Button>Hello world</Button>;
+}
+
+export default Transactions;
