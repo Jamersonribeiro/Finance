9bd0c135369e6ca702324e62aab5344c0c430408
Seu Nome <seuemail@exemplo.com>
2024-11-11 17:55:08 -0300

    feat: create add transaction dialog

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,7 @@
 import { db } from "@/app/_lib/prisma";
-import { ArrowDownUpIcon } from "lucide-react";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
-import { Button } from "../_components/ui/button";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
@@ -12,10 +11,7 @@ const TransactionsPage = async () => {
       {/* titulo e botao*/}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full font-bold">
-          <ArrowDownUpIcon className="ml-2" />
-          Adicionar Transação
-        </Button>
+        <AddTransactionButton />
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
     </div>
