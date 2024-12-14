9f8060cfa3c5aa7d2a7a44666074eda60c65d236
Seu Nome <seuemail@exemplo.com>
2024-12-14 14:22:17 -0300

    feat: adicionado year o schema IA

--- app/(home)/_actions/generate-ai-report/schema.ts
+++ app/(home)/_actions/generate-ai-report/schema.ts
@@ -2,7 +2,12 @@ import { isMatch } from "date-fns";
 import { z } from "zod";
 
 export const generateAiReportSchema = z.object({
-  month: z.string().refine((value) => isMatch(value, "MM")),
+  month: z.string().refine((value) => isMatch(value, "MM"), {
+    message: "Month must be in MM format",
+  }),
+  year: z.string().refine((value) => isMatch(value, "yyyy"), {
+    message: "Year must be in yyyy format",
+  }),
 });
 
 export type GenerateAiReportSchema = z.infer<typeof generateAiReportSchema>;
