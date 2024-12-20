4ffa980eb54e627828b88c549d947b9865143003
Seu Nome <seuemail@exemplo.com>
2024-12-20 10:21:23 -0300

    feat: responsividade navbar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -8,9 +8,9 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <nav className="flex items-center justify-between border-b border-solid px-8 py-4">
+    <nav className="flex flex-wrap items-center justify-between border-b border-solid px-8 py-4">
       {/* ESQUERDA */}
-      <div className="flex items-center gap-10">
+      <div className="flex min-w-0 items-center gap-10">
         <Image src="/logo.png" width={173} height={39} alt="Finance AI" />
         <Link
           href="/"
@@ -44,7 +44,7 @@ const Navbar = () => {
         </Link>
       </div>
       {/* DIREITA - UserButton */}
-      <div className="ml-auto">
+      <div className="ml-auto mt-2 sm:mt-0">
         <UserButton showName />
       </div>
     </nav>
