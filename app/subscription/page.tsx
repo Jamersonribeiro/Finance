8bd57023882f808a191fc18caa154f4f8b608447
Seu Nome <seuemail@exemplo.com>
2025-01-16 17:57:10 -0300

    feat: reduzido a qtde de registros por mês para não assinantes

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -38,7 +38,7 @@ const SubscriptionPage = async () => {
               <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
                 <p>
-                  Apenas 10 transações por mês ({currentMonthTransactions}/10)
+                  Apenas 8 transações por mês ({currentMonthTransactions}/8)
                 </p>
               </div>
               <div className="flex items-center gap-2">
